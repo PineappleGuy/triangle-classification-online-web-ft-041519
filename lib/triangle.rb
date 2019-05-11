13aac8f63939a95eb56c06bc1b34c81fef952f59
Garth Lilly <garff247@gmail.com>
2019-05-11 22:43:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,9 +6,7 @@ class Triangle
     if (side1 + side2) <= side3 || (side1 + side3) <= side2 || (side3 + side2) <= side1 || side1 <= 0 || side2 <= 0 || side3 <= 0
       begin
         raise TriangleError
-      rescue TriangleError => error
       end
-      puts "hi"
     elsif side1 == side2 && side1 == side3
       self.kind = :equilateral
     elsif (side1 == side2 && side1 != side3) || (side1 == side3 && side1 != side2) || (side2 == side3 && side2 != side1)

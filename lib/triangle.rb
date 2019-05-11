2cc8f40fc4f1e38d3a72cb03dc46783bad30d6a3
Garth Lilly <garff247@gmail.com>
2019-05-11 22:33:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
       self.kind = :isosceles
     elsif side1 != side2 && side1 != side3 && side2 != side3
       self.kind = :scalene
-    elsif side1 + side2 <= side3 || side1 <= 0 || side2 <= 0 || side3 <= 0
+    elsif (side1 + side2) <= side3 || side1 <= 0 || side2 <= 0 || side3 <= 0
       begin
         raise TriangleError
       rescue TriangleError => error
